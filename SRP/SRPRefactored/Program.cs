e6e9db172910bc5faba9326b19294e6201d9af38
kannan <kannans89@gmail.com>
2025-11-24 11:02:10 +0530

    srp htm

---
+++ SRP/SRPRefactored/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using SRPRefactored.Domain;
+using SRPRefactored.Presentation;
+
+var starBucksInvoice = new Invoice(1, "Star bucks", 1000, .20);
+var consolePrinter = new InvoiceFilePrinter();
+consolePrinter.PrintInvoice(starBucksInvoice);
