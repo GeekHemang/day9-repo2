c74202c3cb6c8fe00d4a707e39f54e2e137a3ca5
GeekHemang <garghemang@gmail.com>
2025-11-24 14:54:56 +0530

    comments

--- SRP/SRPRefactored/Presentation/InvoiceHtmlPrinter.cs
+++ SRP/SRPRefactored/Presentation/InvoiceHtmlPrinter.cs
@@ -1,30 +1,31 @@
 ﻿using SRPRefactored.Domain;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace SRPRefactored.Presentation
 {
-    class InvoiceFilePrinter
+    class InvoiceHtmlPrinter
     {
-
         public static void PrintInvoice(Invoice invoice)
         {
             string template = $"Id :{invoice.Id} " +
-                $",description:{invoice.Description}" +
+                $" description:{invoice.Description}" +
                 $" cost is :{invoice.Cost}" +
                 $" tax amt is :{invoice.CalcuateTax()}" +
                 $" discount amt is {invoice.CalculateDiscount()}" +
                 $" Final cost is :{invoice.FinalCost()}";
 
-
-            StreamWriter sw = new StreamWriter($"{invoice.Id}.txt");
-
-            sw.WriteLine( template );
+             
+            StreamWriter sw = new StreamWriter($"{invoice.Description}.html"); // To create the txt file in the bin folder.
+            sw.WriteLine(template);
             sw.Close();
+
+            //Create for the Html print
 
         }
     }
--- SRP/SRPRefactored/Program.cs
+++ SRP/SRPRefactored/Program.cs
@@ -3,5 +3,13 @@ using SRPRefactored.Domain;
 using SRPRefactored.Presentation;
 
 var starBucksInvoice = new Invoice(1, "Star bucks", 1000, .20);
-var consolePrinter = new InvoiceFilePrinter();
-consolePrinter.PrintInvoice(starBucksInvoice);
+
+//Its done thorugh creating an instance of PrintInvoice
+//var consolePrinter = new InvoiceFilePrinter();
+//consolePrinter.PrintInvoice(starBucksInvoice);
+
+//This is done by calling thorugh a class which is static
+InvoiceFilePrinter.PrintInvoice(starBucksInvoice);
+
+InvoiceHtmlPrinter.PrintInvoice(starBucksInvoice);
+
